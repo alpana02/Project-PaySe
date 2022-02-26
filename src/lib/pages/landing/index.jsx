58ad9f237b8a481fdd628ed4aed66970a4e025c0
Alpana <alpana.nanda12@gmail.com>
2022-02-26 17:44:05 +0530

    feat: UnBanked Lending

--- src/lib/pages/landing/index.jsx
+++ src/lib/pages/landing/index.jsx
@@ -5,25 +5,73 @@ import { FaAddressCard, FaBacon } from "react-icons/fa";
 import { Heading } from "@chakra-ui/react";
 import { Grid, GridItem } from "@chakra-ui/react";
 import NextLink from "next/link";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Lorem,
+} from "@chakra-ui/react";
 
 const landingPage = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <div className="container">
       <Stack spacing={10}>
         <Heading as="h2" size="xl" isTruncated>
           Choose the Bank to apply loan
         </Heading>
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalCloseButton />
+            <ModalBody>
+            <NextLink href="/uploadBill" passHref>
+                <Link>
+                  <GridItem w="100%">
+                    <Button
+                      marginTop={5}
+                      colorScheme="purple"
+                      onClick={onOpen}
+                      leftIcon={<FaAddressCard />}
+                    >
+                      Unbanked
+                    </Button>
+                  </GridItem>
+                </Link>
+              </NextLink>
+              <NextLink href="/loanapply" passHref>
+                <Link>
+                  <GridItem w="100%">
+                    <Button
+                      marginTop={5}
+                      marginBottom={10}
+                      colorScheme="purple"
+                      onClick={onOpen}
+                      leftIcon={<FaAddressCard />}
+                    >
+                      Continue with Bank account
+                    </Button>
+                  </GridItem>
+                </Link>
+              </NextLink>
+            </ModalBody>
+          </ModalContent>
+        </Modal>
         <Grid templateColumns="repeat(5, 1fr)" gap={6}>
-          <NextLink href='/loanapply' passHref>
-            <Link>
               <GridItem w="100%">
-                <Button colorScheme="purple" leftIcon={<FaAddressCard />}>
+                <Button
+                  colorScheme="purple"
+                  onClick={onOpen}
+                  leftIcon={<FaAddressCard />}
+                >
                   MeraBankFiu
                 </Button>
               </GridItem>
-            </Link>
-          </NextLink>
-
           <GridItem w="100%">
             <Button colorScheme="cyan" leftIcon={<FaBacon />}>
               SBI
